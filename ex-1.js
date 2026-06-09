660551929a81759be6a8567146edff94ef39d9fe
Pongsakorn Yuoeang <bomaiaiza@gmail.com>
2026-06-10 02:27:25 +0700

    fix: correct employee object properties and improve console output

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,13 @@
 // Exercise #1: Food Order
-
+let foodOrder ={
+    orderNumber:"A0234",
+    address:"Bangkok",
+    paymenttype:"Cash",
+    totalPrice: 4500,
+    restaurantName:"MK"
+}
 // Start coding here
+foodOrder.totalPrice = 5000
+foodOrder.paymenttype = "Credit Card"
+console.log(foodOrder)
+
--- ex-3.js
+++ ex-3.js
@@ -1,29 +1,16 @@
 // Exercise #3 Debugging
-
-// Start coding here
 const employee = {
-  name: "John Doe",
-  age: 18,
-  address: {
-    street: "123 Main Street",
-    city: "Cityville",
-    states: "Stateville",
-    country: "Countryland",
-    postalCode: "12345",
-  },
+    name: "John Doe",
+    age: 18,
+    address: {
+        street: "123 Main Street",
+        city: "Cityville",
+        state: "Stateville", // 1. แก้ไขจาก states เป็น state
+        country: "Countryland",
+        postalCode: "12345",
+    },
 };
 
-console.log("Employee Name: " + employees.name);
+console.log("Employee Name: " + employee.name); // 2. แก้ไขจาก employees เป็น employee
 console.log("Employee Age: " + employee.age);
-console.log(
-  "Employee Address: " +
-    employee.address.street +
-    ", " +
-    employee.address.city +
-    ", " +
-    employee.address.state +
-    ", " +
-    employee.address.country +
-    " " +
-    employee.adress.postalCode
-);
+console.log("Employee Address: " + employee.address.street + ", " + employee.address.city + ", " + employee.address.state + ", " + employee.address.country + " " + employee.address.postalCode); // 3. แก้ไขจาก adress เป็น address
